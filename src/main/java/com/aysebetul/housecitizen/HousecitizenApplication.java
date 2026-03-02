edd0b944dbdeba9c8a15973ad2ae87613eb3fcb7
aysebetultat <aaysebetultat@gmail.com>
2026-03-02 21:23:14 +0300

    init

---
+++ src/main/java/com/aysebetul/housecitizen/HousecitizenApplication.java
@@ -0,0 +1,13 @@
+package com.aysebetul.housecitizen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousecitizenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousecitizenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aysebetul/housecitizen/HousecitizenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aysebetul.housecitizen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousecitizenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
